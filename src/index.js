941d925e0a15b3607794089db8d5c7bd9c930803
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-11 13:01:38 -0300

    config project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import React, { StrictMode  } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
